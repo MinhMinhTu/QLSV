02f5c0b0b762e2d5190c25fc8cba1b5f75afddea
tu <you@example.com>
2019-03-07 11:29:47 +0700

    V9

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery'
-import 'bootstrap/dist/js/bootstrap.min.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'jquery'
+// import 'bootstrap/dist/js/bootstrap.min.js';
 
 ReactDOM.render(<App/> , document.getElementById('root'))   
 
